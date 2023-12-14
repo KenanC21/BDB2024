4187d5b3f0e26324145efd980996ee589a637779
Michael Egle <mjegle@iastate.edu>
2023-12-13 23:10:40 -0600

    updated angle code

--- code/util/create_and_standardize_week_data.R
+++ code/util/create_and_standardize_week_data.R
@@ -31,20 +31,15 @@ create_and_standardize_week_data <- function(week)
     # get rid of the ball
     filter(!is.na(nflId)) %>% 
     # use geometry to find the angle created by the defender, each possible blocker, and the ball carrier
-    # TODO - I *think* this is calculating the angle that we should be concerned with, but not entirely sure
-    mutate(distance_to_possible_blocker = sqrt((x - possible_blocker_x)^2 + (y - possible_blocker_y)^2),
-           # do some geometry
-           distance_to_ball_carrier_x = x - ball_carrier_x,
-           distance_to_ball_carrier_y = y - ball_carrier_y,
-           distance_to_possible_blocker_x = possible_blocker_x - x,
-           distance_to_possible_blocker_y = possible_blocker_y - y,
-           # refresher: cos(theta) = (a * b) / (||a||||b||)
-           # where a and b are two vectors
-           # in this case we set a to be the vector formed by the defender and ball carrier
-           # and b to be the vector formed by the defender and the possible blocker
-           theta = acos(((distance_to_ball_carrier_x * distance_to_possible_blocker_x) + 
-                           (distance_to_ball_carrier_y * distance_to_possible_blocker_y)) / 
-                          (distance_to_ball_carrier * distance_to_possible_blocker)),
+    # TODO - maybe try the angles from other points, see if those yield any interesting results
+    mutate(distance_blocker_to_defender = sqrt((possible_blocker_x - x)^2 + (possible_blocker_y - y)^2),
+           distance_blocker_to_ball_carrier = sqrt((ball_carrier_x - possible_blocker_x)^2 + (ball_carrier_y - possible_blocker_y)^2),
+           theta = acos((distance_blocker_to_defender^2 + distance_blocker_to_ball_carrier^2 - distance_to_ball_carrier^2) /
+                          (2 * distance_blocker_to_defender * distance_blocker_to_ball_carrier)),
+           # refresher: law of cosines says cos(b) = (c^2 + a^2 - b^2) / 2*a*c
+           # where a is the distance from possible blocker to ball carrier
+           # b is the distance from defender to ball carrier
+           # and c is the distance from possible blocker to defender
            theta = ifelse(theta > pi, theta - pi, theta)) %>% 
     group_by(gameId, playId, nflId, frameId) %>%
     # TODO - this can have a little more done on it, should ideally be a summarize()
@@ -52,20 +47,19 @@ create_and_standardize_week_data <- function(week)
     # blocker locations as possible
     summarize(across(c(x, y, s, a, dir, 
                        will_have_chance_to_make_tackle, will_make_tackle,
-                       distance_to_ball_carrier_x,
-                       distance_to_ball_carrier_y, distance_to_ball_carrier,
+                       distance_to_ball_carrier,
                        ball_carrier_distance_to_sideline,
                        ball_carrier_distance_to_endzone, ball_carrier_s, 
                        ball_carrier_a, ball_carrier_dir,
                        ball_carrier_a_difference, ball_carrier_s_difference,
                        ball_carrier_dir_difference),
                      first),
-              max_distance_from_blocker = max(distance_to_possible_blocker),
-              possible_blockers_within_3_yards = sum(distance_to_possible_blocker <= 3),
-              possible_blockers_within_4_yards = sum(distance_to_possible_blocker <= 4),
-              possible_blockers_within_5_yards = sum(distance_to_possible_blocker <= 5),
-              possible_blockers_within_6_yards = sum(distance_to_possible_blocker <= 6),
-              possible_blockers_within_7_yards = sum(distance_to_possible_blocker <= 7),
+              max_distance_from_blocker = max(distance_blocker_to_defender),
+              possible_blockers_within_3_yards = sum(distance_blocker_to_defender <= 3),
+              possible_blockers_within_4_yards = sum(distance_blocker_to_defender <= 4),
+              possible_blockers_within_5_yards = sum(distance_blocker_to_defender <= 5),
+              possible_blockers_within_6_yards = sum(distance_blocker_to_defender <= 6),
+              possible_blockers_within_7_yards = sum(distance_blocker_to_defender <= 7),
               possible_blockers_with_over_90_degree_angle = sum(theta >= pi/2),
               possible_blockers_with_over_135_degree_angle = sum(theta >= 3 * pi / 4),
               max_angle_formed_by_blocker_and_ball_carrier = max(theta)) %>% 
