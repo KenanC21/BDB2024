95de3efdfa9109f30aff678166eb93c40ef24b2e
Michael Egle <mjegle@iastate.edu>
2023-12-16 00:33:02 -0600

    try again

--- code/modeling/tackle_participation_scratch.R
+++ code/modeling/tackle_participation_scratch.R
@@ -1,12 +1,20 @@
 library(caret)
 library(xgboost)
 library(pROC)
+library(ROCR)
+library(ROCit)
 library(tidyverse)
 library(tidymodels)
 library(probably)
 # source("code/data_manipulation/tackle_model_data_manipulation.R")
 
+setwd("/Users/michaelegle/BDB2024")
+
 source("code/util/create_and_standardize_week_data.R")
+
+
+plays <- read_csv("data/plays.csv")
+tackles <- read_csv("data/tackles.csv")
 
 
 # Preliminary data split for testing and training 
@@ -77,19 +85,45 @@ pred_test
 # Convert prediction type to binomial response with a cutoff of 0.5
 # meaning our model will focus on players with at least a 50% chance of
 # making the tackle, missing or assisting 
-pred_test_fact = factor(ifelse(pred_test>=.5, 1, 0))
+pred_test_fact = factor(ifelse(pred_test >= 0.36, 1, 0))
 
 
 # Append factored reponse of having chance to make a tackle 
 pred <- testing(prelim_split) %>% 
-  bind_cols(pred = pred_test_fact)
+  bind_cols(pred = pred_test_fact) %>% 
+  bind_cols(prob = pred_test)
 
 
 # 95% confident the accuracy of our model will predict a player having a chance
 # to make the tackle to be between .813 and .8842
-conf_mat = confusionMatrix(as.factor(as.numeric(test_y)), pred_test_fact)
+conf_mat = confusionMatrix(as.factor(as.numeric(test_y), levels = c(0, 1)), pred_test_fact)
 print(conf_mat)
 
-plot.roc(test_y,  pred_test)
+# Since we're imbalanced, we need to consider not just the sensitivity/specificity,
+# but the negative and positive prediction values as well
+
+table(pred_test_fact, pred$will_have_chance_to_make_tackle)
+
+# While this is useful, this label is showing the NPV and PPV, not specificity/sensitivity
+plot.roc(test_y,  pred_test, print.thres = T, print.auc = T)
+
+ROCit_obj <- rocit(score = pred$prob ,class = pred$will_have_chance_to_make_tackle)
+plot(ROCit_obj)
+ksplot(ROCit_obj)
+
+ROCit_obj$Cutoff
+
+cutoff_df <- data.frame(cutoff = ROCit_obj$Cutoff,
+                        FPR = ROCit_obj$FPR,
+                        TPR = ROCit_obj$TPR)
+
+cal_plot_windowed(.data = pred,
+                  truth = will_have_chance_to_make_tackle,
+                  estimate = prob,
+                  window_size = 0.05)
+
+# Good looking calibration plot, just hope we're not overfitting
+# may have to do additional tests for that
+
 
 
--- code/modeling/train_participation_model.R
+++ code/modeling/train_participation_model.R
@@ -12,8 +12,6 @@
 #'
 train_participation_model <- function(train_weeks)
 {
-  # TODO
-  
   all_participation_predictions <- pmap_dfr(.l = list(1:9),
                                             .f = train_participation_model_helper,
                                             train_weeks = train_weeks)
@@ -31,14 +29,57 @@ train_participation_model <- function(train_weeks)
 #' @return - predictions for the held out week, based on a model trainined on the
 #'           remaining weeks
 #'
-train_participation_model_helper <- function(train_weeks, week)
+train_participation_model_helper <- function(train_weeks, holdout_week)
 {
   # Filter out the week to keep as a holdout week since we want out of sample predictions
   
-  # test_set <- week %>% 
-  #   filter(week == week)
-  
   print(paste("Holding out week", week))
   
-  return(data.frame())
+  # test_set <- week %>% 
+  #   filter(week == week)
+  train <- train_weeks %>% 
+    filter(week != holdout_week)
+  
+  test <- train_weeks %>% 
+    filter(week == holdout_week)
+  
+  train_x <- train %>% #Independent variables for train
+    select(-will_have_chance_to_make_tackle) %>% 
+    as.matrix()
+  
+  train_y <- train %>% #Dependent variable for train
+    select(will_have_chance_to_make_tackle) %>% 
+    as.matrix()
+  
+  test_x <- test %>% #Independent vars for test
+    select(-will_have_chance_to_make_tackle) %>% 
+    as.matrix()
+  
+  test_y <- test %>% #Dependent var for test
+    select(will_have_chance_to_make_tackle) %>% 
+    as.matrix()
+  
+  # Convert train and test data in xgboost matrix
+  xgboost_train = xgb.DMatrix(data=train_x, label=train_y)
+  xgboost_test = xgb.DMatrix(data=test_x, label=test_y)
+  
+  # Creating training model
+  # tictoc::tic()
+  participation_model_iter <- xgboost(data = xgboost_train,
+                                      # TODO - update these to be optimal parameters
+                                      max.depth=3,                           
+                                      nrounds=100,
+                                      params = list(objective = "binary:logistic"),
+                                      eval_metric = 'auc') 
+  # tictoc::toc()
+  
+  pred_test <- predict(participation_model_iter, xgboost_test)
+  # TODO - replace this with the actual value we decide on
+  # for decision threshold
+  pred_tackle_participation <- ifelse(pred_test >= 0.138, 1, 0)
+  
+  final_df <- test %>% 
+    bind_cols(pred)
+  
+  return(final_df)
 }
